1a9a60ea21ffae68c759560fca746aa0b0b02b21
beefycheddar <brettgavin@live.com>
2024-03-26 10:26:57 -0700

    Update IVR2HV_w_VM_Folders.ps1
    additional notes added

--- IVR2HV_w_VM_Folders.ps1
+++ IVR2HV_w_VM_Folders.ps1
@@ -12,6 +12,16 @@
     CONTACT: brett.gavin@veeam.com
     LASTEDIT: 3-26-2024
     KEYWORDS: Hyper-V, Instant VM Restore, VM Subfolders
+    
+    In this script:
+
+	$sourceVMs retrieves the VMs from the Veeam backup job.
+	$targetHyperVHost and $targetHyperVDatastore specify the Hyper-V host and datastore where the VMs will be restored.
+	Inside the foreach loop, $vm.Name is used to get the name of each VM being restored.
+	$folderName specifies the name of the subfolder for each VM. You can modify this to include any naming convention you prefer.
+	Start-VBRVMRestoreToHyperV initiates the restore operation for each VM, specifying the target Hyper-V host, datastore, and the folder where the VM should be restored.
+	By using this approach, you can create a subfolder for each VM being restored during the migration from VMware to Hyper-V, helping to organize the restored VMs for better management and identification.
+
 #> 
 
 
